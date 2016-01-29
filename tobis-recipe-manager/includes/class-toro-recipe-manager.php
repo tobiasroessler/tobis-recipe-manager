69b36edf7dbae5f051cd87e7e3d91fc21b772bab
torox <torox@torox>
2016-01-29 23:21:29 +0100

    Plugin works with objects now

--- tobis-recipe-manager/includes/class-toro-recipe-manager.php
+++ tobis-recipe-manager/includes/class-toro-recipe-manager.php
@@ -2,19 +2,18 @@
 
 class TORO_Recipe_Manager {
     
-    const NAME = "Recipes";
-    const MENU_NAME = "Recipes";
-    const ADD_NEW = "Add New";
-    const EDIT_RECIPE = "Edit Recipe";
-    const NEW_RECIPE = "New Recipe";
-    const VIEW_RECIPE = "View Recipe";
-    const SEARCH_RECIPES = "Search Recipes";
-    const NOT_FOUND = "No recipes found";
-    const NOT_FOUND_IN_TRASH = "No recipes found in trash";
-    
-    const RECIPE_POST_TYPE_NAME = "tobis_recipes";
+    const RECIPE_POST_TYPE_ID = "tobis_recipes";
+    const RECIPE_POST_TYPE_NAME = "Recipes";
     const RECIPE_POST_TYPE_DESC = "Tobi's Recipes";
-    const MENU_POSITION = 8;
+    const RECIPE_POST_TYPE_MENU_NAME = "Recipes";
+    const RECIPE_POST_TYPE_ADD_NEW = "Add New";
+    const RECIPE_POST_TYPE_EDIT_RECIPE = "Edit Recipe";
+    const RECIPE_POST_TYPE_NEW = "New Recipe";
+    const RECIPE_POST_TYPE_VIEW = "View Recipe";
+    const RECIPE_POST_TYPE_SEARCH = "Search Recipes";
+    const RECIPE_POST_TYPE_MENU_POSITION = 8;
+    const RECIPES_NOT_FOUND = "No recipes found";
+    const RECIPES_NOT_FOUND_IN_TRASH = "No recipes found in trash";
     
     const INGREDIENTS_PARENT_SLUG = "edit.php?post_type=tobis_recipes";
     const INGREDIENTS_PAGE_TITLE = "Ingredients";
@@ -22,7 +21,6 @@ class TORO_Recipe_Manager {
     const INGREDIENTS_CAPABILITY = "activate_plugins";
     const INGREDIENTS_MENU_SLUG = "tobis_recipes_ingredients";
     const INGREDIENTS_FUNCTION = "my_render_list_page";
-    
     
     function __construct() {
         // Do nothing
@@ -42,17 +40,17 @@ class TORO_Recipe_Manager {
     
     function register_recipes_post_type() {
         $labels = array(
-            'name' => _x(self::NAME, 'tobis_recipe_manager'),
-            'singular_name' => _x(self::NAME, 'tobis_recipe_manager'),
-            'add_new' => _x(self::ADD_NEW, 'tobis_recipe_manager'),
-            'add_new_item' => _x(self::ADD_NEW, 'tobis_recipe_manager'),
-            'edit_item' => _x(self::EDIT_RECIPE, 'tobis_recipe_manager'),
-            'new_item' => _x(self::NEW_RECIPE, 'tobis_recipe_manager'),
-            'view_item' => _x(self::VIEW_RECIPE, 'tobis_recipe_manager'),
-            'search_items' => _x(self::SEARCH_RECIPES, 'tobis_recipe_manager'),
-            'not_found' => _x(self::NOT_FOUND, 'tobis_recipe_manager'),
-            'not_found_in_trash' => _x(self::NOT_FOUND_IN_TRASH, 'tobis_recipe_manager'),
-            'menu_name' => _x(self::MENU_NAME, 'tobis_recipe_manager'),
+            'name' => _x(self::RECIPE_POST_TYPE_NAME, 'tobis_recipe_manager'),
+            'singular_name' => _x(self::RECIPE_POST_TYPE_NAME, 'tobis_recipe_manager'),
+            'add_new' => _x(self::RECIPE_POST_TYPE_ADD_NEW, 'tobis_recipe_manager'),
+            'add_new_item' => _x(self::RECIPE_POST_TYPE_ADD_NEW, 'tobis_recipe_manager'),
+            'edit_item' => _x(self::RECIPE_POST_TYPE_EDIT_RECIPE, 'tobis_recipe_manager'),
+            'new_item' => _x(self::RECIPE_POST_TYPE_NEW, 'tobis_recipe_manager'),
+            'view_item' => _x(self::RECIPE_POST_TYPE_VIEW, 'tobis_recipe_manager'),
+            'search_items' => _x(self::RECIPE_POST_TYPE_SEARCH, 'tobis_recipe_manager'),
+            'not_found' => _x(self::RECIPES_NOT_FOUND, 'tobis_recipe_manager'),
+            'not_found_in_trash' => _x(self::RECIPES_NOT_FOUND_IN_TRASH, 'tobis_recipe_manager'),
+            'menu_name' => _x(self::RECIPE_POST_TYPE_MENU_NAME, 'tobis_recipe_manager'),
         );
 
         $args = array(
@@ -61,12 +59,12 @@ class TORO_Recipe_Manager {
             'description' => self::RECIPE_POST_TYPE_DESC,
             'public' => true,
             'show_in_menu' => true,
-            'menu_position' => self::MENU_POSITION,
+            'menu_position' => self::RECIPE_POST_TYPE_MENU_POSITION,
             'has_archive' => true,
             'rewrite' => true,
         );
         
-        register_post_type(self::RECIPE_POST_TYPE_NAME, $args);
+        register_post_type(self::RECIPE_POST_TYPE_ID, $args);
     }
 
     function add_submenu_for_ingredients(){
